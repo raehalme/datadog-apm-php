7c0ba1e27029c05540c99a2520e5ce82eda81979
Thomas Raehalme <thomas@raehalme.net>
2022-04-28 12:15:12 +0300

    Refactored index.php into functions.

--- src/php/index.php
+++ src/php/index.php
@@ -1,41 +1,50 @@
 <?
 
-$conn = new mysqli("mariadb", "root", "secret", "db_name");
-
-$conn->query("CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT, name VARCHAR(8), PRIMARY KEY (id))");
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+function insertUser($conn) {
 	$stmt = $conn->prepare("INSERT INTO users(name) VALUES(?)");
 	$stmt->bind_param("s", $_POST["name"]);
 	$stmt->execute();
-	header("Location: index.php");
-	die();
 }
-else if (!empty($_GET["delete"])) {
+
+function deleteUser($conn) {
 	$stmt = $conn->prepare("DELETE FROM users WHERE id = ?");
 	$stmt->bind_param("i", $_GET["id"]);
 	$stmt->execute();
-	header("Location: index.php");
-	die();
 }
-else {
+
+function listUsers($conn) {
 	$result = $conn->query("SELECT id, name FROM users");
-	if ($result->num_rows > 0) {
-		// output data of each row
+	if ($result->num_rows == 0) {
+		echo "<p>0 results</p>";
+	}
+	else {
 		while($row = $result->fetch_assoc()) {
 			echo $row['name'];
 			echo "<form method=\"get\"><input type=\"hidden\" name=\"id\" value=\"" . $row["id"] . "\"/><input type=\"submit\" name=\"delete\" value=\"Delete\" /></form>";
 			echo "<br>";
 		}
 	}
-	else {
-		echo "<p>0 results</p>";
-	}
+
+	echo "<br><form method=\"post\">";
+	echo "Name: <input name=\"name\" />";
+	echo "<input type=\"submit\" value=\"Add\" />";
+	echo "</form>";
 }
 
-echo "<br><form method=\"post\">";
-echo "Name: <input name=\"name\" />";
-echo "<input type=\"submit\" value=\"Add\" />";
-echo "</form>";
+$conn = new mysqli("mariadb", "root", "secret", "db_name");
+
+$conn->query("CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT, name VARCHAR(8), PRIMARY KEY (id))");
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	insertUser($conn);
+	header("Location: index.php");
+}
+else if (!empty($_GET["delete"])) {
+	deleteUser($conn);
+	header("Location: index.php");
+}
+else {
+	listUsers($conn);
+}
 
 $conn->close();
